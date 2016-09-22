c39f7718c4b1800b90f85a014d2c61be60913363
Uriel Infante <uriel@localhost.localdomain>
2016-09-21 20:38:45 -0500

    Configurando entorno Github/Heroku en Debian

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -36,7 +36,7 @@
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-md-6">
-                                Pedidos
+                                Pedidos En Camino
                             </div>
                         </div>
                     </div>
