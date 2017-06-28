ac8a8071587bf6e589e1caf1947f15d0a844952a
Uriel <ur13l.infante@gmail.com>
2017-06-28 12:10:26 -0500

    Cambio de Middleware para https

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-      if(!$request->secure()) {
+      if(Request::secure()) {
         return redirect()->secure('/');
       }
         return view('pedidos.index');
