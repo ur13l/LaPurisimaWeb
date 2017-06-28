dfcb7bb025c817f6738270505706a13a9053ebe7
Uriel <ur13l.infante@gmail.com>
2017-06-28 11:38:10 -0500

    Cambio de Middleware para https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,6 +15,7 @@ class HttpsProtocol
      */
      public function handle($request, Closure $next)
      {
+          if(!$request->secure())
                  return redirect()->secure($request->getRequestUri());
 
              return $next($request);
